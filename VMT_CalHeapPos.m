40b12f0398694b1b086a3f65fbaaf4d506e6a43d
Freezing0325 <1603441246@qq.com>
2024-05-20 17:07:14 +0800

    利用VMT_SingleGetU函数优化VMT_CalHeapPos

--- VMT_CalHeapPos.m
+++ VMT_CalHeapPos.m
@@ -18,16 +18,9 @@ function [RealK, HeapPos] = VMT_CalHeapPos(NormK, CompStatus, CalMethod, ActiveS
     elseif (CalMethod == 2)
         theta_m_Mat = atan(sqrt((1 + HMat.^2).^(1/3) - 1));
         U_snap_Mat = HMat - sqrt((1 + HMat.^2).^(1/3) - 1);
-        Func_Mat = zeros(2, 2);
-        Func_Mat(:, 1) = -HMat.'.^2 ./ (HMat.'.^2 + 1) .^ (3/2);
-        Func_Mat(:, 2) = -1.5 * HMat.' ./ (HMat.'.^2 + 1) .^ (5/2);
         F_snap_Mat = sin(theta_m_Mat) - tan(theta_m_Mat) .* cos(theta_0_Mat);
     elseif (CalMethod == 3)
         U_snap_Mat = HMat - HMat ./ sqrt(2*HMat.^2 + 3);
-        Func_Mat = zeros(2, 3);
-        Func_Mat(:, 1) = 2*HMat.^2 ./ (HMat.^2 + 1).^(3/2);
-        Func_Mat(:, 2) = (3*HMat.*(HMat.^2 - 1)) ./ (HMat.^2 + 1).^(5/2);
-        Func_Mat(:, 3) = (4*HMat.^4 - 10*HMat.^2 + 1) ./ (HMat.^2 + 1).^(7/2);
         F_snap_Mat = 2 * HMat.^3 ./ (3 * sqrt(3) .* sqrt(HMat.^2 + 1));
     end
 
@@ -40,7 +33,6 @@ function [RealK, HeapPos] = VMT_CalHeapPos(NormK, CompStatus, CalMethod, ActiveS
     if (~exist('ActiveStatus', 'var') || size(ActiveStatus, 1) == 0)
         ActiveStatus = ones(1, UnitSum);
     end
-    StepSum = sum(ActiveStatus ~= 0);
     for i = 1: UnitSum
         if (~ActiveStatus(i))
             continue;
@@ -59,43 +51,14 @@ function [RealK, HeapPos] = VMT_CalHeapPos(NormK, CompStatus, CalMethod, ActiveS
                 SolutionPos = [-Inf, -HMax_Mat(IfThatComp + 1)] * (IsDown * 2 - 1);
                 U_all(j) = HMat(IfThatComp + 1) - vpasolve(ThisF == RealK(SortIndex(i)) / RealK(SortIndex(j)) * F_snap_Mat(IfThisComp + 1), SolutionPos);
             elseif (CalMethod == 2)
-                Eqn_a = -Func_Mat(IfThatComp + 1, 2);
-                Eqn_b = -Func_Mat(IfThatComp + 1, 1);
-                Eqn_c = -RealK(SortIndex(i)) / RealK(SortIndex(j)) * F_snap_Mat(IfThisComp + 1) * (IsDown * 2 - 1);
-                Eqn_Delta = Eqn_b^2 - 4 * Eqn_a * Eqn_c;
-                if (Eqn_Delta <= 0)
-                    Eqn_a = -(2/(HMat(IfThatComp)^2 + 1)^(5/6) - 5/(2 * (HMat(IfThatComp)^2 + 1)^(7/6)));
-                    Eqn_b = 3 * ((HMat(IfThatComp)^2 + 1)^(1/3) - 1)^(1/2) / (2 * (HMat(IfThatComp)^2 + 1)^(5/6));
-                    Eqn_c = 0;
-                    Eqn_d = RealK(SortIndex(i)) / RealK(SortIndex(j)) * F_snap_Mat(IfThisComp + 1) - F_snap_Mat(IfThatComp + 1);
-                    Eqn_p = Eqn_c / Eqn_a - (Eqn_b / Eqn_a)^2 / 3;
-                    Eqn_q = Eqn_d / Eqn_a + 2 * (Eqn_b / (3 * Eqn_a))^3  - Eqn_b * Eqn_c / (3 * Eqn_a^2);
-                    Eqn_r = sqrt(-(Eqn_p/3)^3);
-                    Eqn_theta = acos(-Eqn_q / (2 * Eqn_r)) / 3;
-                    U_all(j) = HMat(IfThatComp) - sqrt((1 + HMat(IfThatComp)^2).^(1/3) - 1) -(2 * Eqn_r^(1/3) * cos(Eqn_theta) - Eqn_b / (3 * Eqn_a));
-                else
-                    U_all(j) = -(sqrt(Eqn_Delta) - Eqn_b) / (2 * Eqn_a);
-                    if (IsDown)
-                        U_all(j) = 2 * HMat(IfThatComp + 1) - U_all(j);
-                    end
+                U_all(j) = VMT_SingleGetU(RealE(SortIndex(j)), HMat(IfThatComp + 1), RealE(SortIndex(i)) * F_snap_Mat(IfThisComp + 1), IsDown, 2);
+                if (~isreal(U_all(j)))
+                    U_all(j) = VMT_SingleGetU(RealE(SortIndex(j)), HMat(IfThatComp + 1), RealE(SortIndex(i)) * F_snap_Mat(IfThisComp + 1), IsDown, -2);
                 end
-                
             elseif (CalMethod == 3)
-                Eqn_a = -Func_Mat(IfThatComp + 1, 3);
-                Eqn_b = -Func_Mat(IfThatComp + 1, 2);
-                Eqn_c = -Func_Mat(IfThatComp + 1, 1);
-                Eqn_d = -RealK(SortIndex(i)) / RealK(SortIndex(j)) * F_snap_Mat(IfThisComp + 1) * (IsDown * 2 - 1);
-                Eqn_p = Eqn_c / Eqn_a - (Eqn_b / Eqn_a)^2 / 3;
-                Eqn_q = Eqn_d / Eqn_a + 2 * (Eqn_b / (3 * Eqn_a))^3  - Eqn_b * Eqn_c / (3 * Eqn_a^2);
-                if ((Eqn_p / 3)^3 + (Eqn_q / 2)^2 > 0)
-                    U_all(j) = HMat(IfThatComp + 1) - HMax_Mat(IfThatComp + 1) - sqrt((F_snap_Mat(IfThatComp + 1) - Eqn_d) * (3 * HMat(IfThatComp + 1)^2 + 3)^(5/2) / (HMat(IfThatComp + 1) * (2 * HMat(IfThatComp + 1)^2 + 3)^3));
-                else
-                    Eqn_r = sqrt(-(Eqn_p/3)^3);
-                    Eqn_theta = acos(-Eqn_q / (2 * Eqn_r)) / 3;
-                    U_all(j) = 2 * Eqn_r^(1/3) * cos(Eqn_theta - 2 * pi / 3) - Eqn_b / (3 * Eqn_a);
-                    if (IsDown)
-                        U_all(j) = 2 * HMat(IfThatComp + 1) - U_all(j);
-                    end
+                U_all(j) = VMT_SingleGetU(RealE(SortIndex(j)), HMat(IfThatComp + 1), RealE(SortIndex(i)) * F_snap_Mat(IfThisComp + 1), IsDown, 3);
+                if (~isreal(U_all(j)))
+                    U_all(j) = VMT_SingleGetU(RealE(SortIndex(j)), HMat(IfThatComp + 1), RealE(SortIndex(i)) * F_snap_Mat(IfThisComp + 1), IsDown, -3);
                 end
             end
         end
